8337ada1dbf6731b421dae1ec4897ca36cb50406
smailbayali03 <bayalievamadina85@gmail.com>
2026-04-09 00:30:06 +0500

    Fix next.config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      resolveExtensions: ['.tsx', '.ts', '.jsx', '.js'],
-    },
-  },
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/node_modules/**', '**/bot/.wwebjs_auth/**', '**/.git/**'],
-    }
-    return config
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
