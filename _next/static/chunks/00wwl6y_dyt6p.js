870bbf2484932ef96d275be013fb3392485f47a6
smailbayali03 <bayalievamadina85@gmail.com>
2026-04-10 18:39:18 +0500

    Updates

--- _next/static/chunks/00wwl6y_dyt6p.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,67968,t=>{t.v(e=>Promise.all(["static/chunks/0hg2ywi7.emmz.js"].map(e=>t.l(e))).then(()=>e(92599)))}]);
